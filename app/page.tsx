5d638e7438535b6abe0ed416cb09220d975b68ad
S-P-H-E <sphe.g.personal@gmail.com>
2023-08-27 00:18:51 +0200

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 
 export default function Home() {
   const goal = 382000;
-  const current = 354;
+  const current = 372;
   const percentage = ((current / goal) * 100).toFixed(3); // Rounding to 3 decimal places
 
   const formattedGoal = goal.toLocaleString(); // Format the goal number with commas
 
   return (
     <div className='flex flex-col gap-5 justify-center items-center h-screen p-5'>
-      <div className="relative w-full md:w-[500px] h-[200px] flex justify-center items-center m-5 shadow-xl rounded-xl">
+      <div className="relative w-full md:w-[500px] md:h-[200px] flex justify-center items-center m-5 shadow-xl rounded-xl">
         {/* <div className='absolute top-6 bg-gradient-to-t from-black w-full h-full rounded-xl flex justify-center items-end text-white p-5'>
           <h1 className='text-2xl font-medium'>Mclaren 765lt </h1>
         </div> */}

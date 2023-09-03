7fb926b569e8e849f41a0efd87935beed376566d
S-P-H-E <sphe.g.personal@gmail.com>
2023-09-03 10:16:34 +0200

    money update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 
 export default function Home() {
   const goal = 382000;
-  const current = 386;
+  const current = 336.88;
   const percentage = ((current / goal) * 100).toFixed(3); // Rounding to 3 decimal places
 
   const formattedGoal = goal.toLocaleString(); // Format the goal number with commas

50a6d9315bddc5e5bb41b77db2647b38e8288e6a
S-P-H-E <123868239+S-P-H-E@users.noreply.github.com>
2023-09-24 07:45:56 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,6 @@ import Coupe from "@/components/Coupe";
 
 
 export default function Home() {
-  const goal = 382000;
-  const current = 336.88;
-  const percentage = ((current / goal) * 100).toFixed(3); // Rounding to 3 decimal places
-
-  const formattedGoal = goal.toLocaleString(); // Format the goal number with commas
-
   return (
     <Coupe col={[]} />
   );
